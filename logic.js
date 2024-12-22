5856b72d61e3b15947d2fad10bc5ec79073dbdfa
Pulse04 <190657331+Pulse04@users.noreply.github.com>
2024-12-22 20:05:24 +0200

    Update logic.js

--- logic.js
+++ logic.js
@@ -125,8 +125,8 @@ function setUserLocation() {
                     .then(data => {
                         // If the geocoding request returns a valid address
                         const address = data.display_name;
-                        userMarker.bindPopup("Your current location is: " + address + "<!-- New button to create location marker -->" +
-"<button id='create-location-marker'>Create Location Marker</button>").openPopup();
+                        userMarker.bindPopup("Your current location is: " + address + "<strong>Whats going on?</strong>
+" ).openPopup();
 
                     })
                     .catch(error => {
